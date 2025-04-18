ca51ceb7c47d4bc54d0a915ca33b085358b42bc1
biekeleenknegt <117638269+biekeleenknegt@users.noreply.github.com>
2025-04-18 19:08:30 +0000

    ww

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ st.set_page_config(page_title="Project Cost Estimator", layout="centered")
 st.title("Secure Project Cost Estimator")
 
 # Hardcoded login credentials
-USERNAME = "biekeleenknegt"
-PASSWORD = "MaxenLoena2?"
+USERNAME = "test"
+PASSWORD = "wachtwoordvantest?"
 
 # Session state to remember login
 if "authenticated" not in st.session_state:
